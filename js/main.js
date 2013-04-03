b5781035231c0dbffb57a6c8464adf8190071b6e
Andrew Hyder <hyd415@gmail.com>
2013-04-03 16:17:04 -0700

    First commit, figuring out the basics.

---
+++ js/main.js
@@ -0,0 +1,68 @@
+// Get map ready to load
+function initialize() {
+
+  var selectedShape;
+  
+  // Put the map on the page.
+  var mapOptions = {
+    center: new google.maps.LatLng(37.7639, -122.446),
+    zoom: 14,
+    disableDefaultUI: true,
+    mapTypeId: google.maps.MapTypeId.HYBRID
+  };
+
+  var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+  
+  // Drop Google's neighborhood names
+  var mapStyle = [{
+    featureType: "administrative.neighborhood", stylers: [{ visibility: 'off' }]
+  }];
+
+  map.setOptions({styles: mapStyle});
+  
+  // Load up drawing library
+  var drawingManager = new google.maps.drawing.DrawingManager({
+    drawingControl: true,
+    drawingControlOptions: {
+      position: google.maps.ControlPosition.TOP_LEFT,
+      drawingModes: [google.maps.drawing.OverlayType.POLYGON]
+    },
+
+    polygonOptions: {
+      fillColor: '#0099FF',
+      fillOpacity: 0.7,
+      strokeColor: '#AA2143',
+      strokeWeight: 2,
+      clickable: true,
+      zIndex: 1,
+      editable: true
+    }
+  });
+
+  drawingManager.setMap(map);
+
+  // Clear selection if the user clicks the map, not a hood
+  function clearSelection() {
+    if (!selectedShape) return;
+    selectedShape.setEditable(false);
+    selectedShape = null;
+  }
+
+  // Set current shape as the selected one.
+  function setSelection(shape) {
+    clearSelection();
+    selectedShape = shape;
+    shape.setEditable(true);
+  }
+
+  // When you finish drawing the polygon, deselect it. TODO: Add a id, name to the polygon.
+  google.maps.event.addListener(drawingManager, 'overlaycomplete', function(e) {
+      var newShape = e.overlay;
+      newShape.type = e.type;
+      newShape.setEditable(false);
+    });
+
+  drawingManager.addListener(map, 'click', clearSelection);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
